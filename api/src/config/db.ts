f523f310e6a589b10237b4e71d941e1da3bc4476
goodfoodtruck <mielatomique@protonmail.com>
2026-03-31 10:34:01 +0200

    add db

---
+++ api/src/config/db.ts
@@ -0,0 +1,20 @@
+import { DataSource } from 'typeorm';
+
+const AppDataSource = new DataSource({
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  synchronize: false,       // ← ne jamais mettre true en prod
+  logging: process.env.NODE_ENV === "development",
+  entities: ["src/entities/**/*.js"],
+  migrations: ["src/migrations/**/*.js"],
+});
+
+export async function connectDatabase() {
+  try {
+    await AppDataSource.initialize();
+    console.log("✅ Connecté à PostgreSQL via TypeORM");
+  } catch (err: unknown) {
+    console.error("❌ Échec connexion TypeORM :", err);
+    setTimeout(connectDatabase, 5000);
+  }
+}
--- api/src/index.ts
+++ api/src/index.ts
@@ -2,23 +2,26 @@ import express from 'express';
 import morgan from "morgan"
 import logger from './logger';
 import { errorMiddleware } from './middlewares/errorMiddleware';
+import { connectDatabase } from './config/db';
 
 const main = async() => {
-    const app = express()
-    const PORT = 8000
+    const app = express();
+    const PORT = 8000;
 
-    app.use(express.json())
+    await connectDatabase();
+
+    app.use(express.json());
     app.use(express.static('src/public'));
     const stream = {
         write: (message: string) => logger.info(message.trim())
     }
 
-    app.get("/", (req, res) => res.status(200).send({ message: "Test API." }))
+    app.get("/", (req, res) => res.status(200).send({ message: "Test API." }));
 
-    app.use(morgan('combined', { stream }))
+    app.use(morgan('combined', { stream }));
     
-    app.use(errorMiddleware)
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+    app.use(errorMiddleware);
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
 main()
