39efd4449615c954c2c9f57575ce34ed617a298f
Celina CHABANE <celina.chabane@acensi.fr>
2026-04-01 10:34:06 +0200

    Add Store REDUX

---
+++ applications/Frontend/src/store/slices/userSlice.ts
@@ -0,0 +1,68 @@
+import UserService from '@/services/user/userService'
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+export interface UserData {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  automobile: string;
+  electric: boolean;
+  parked: boolean;
+}
+
+interface UserState {
+  user: UserData | null,
+  status: 'pending' | 'fulfilled' | 'rejected',
+  isLoading: boolean
+}
+
+const initialState: UserState = {
+  user: null,
+  status: 'fulfilled',
+  isLoading: false
+}
+
+export const fetchCurrentUser = createAsyncThunk(
+  'auth/fetchCurrentUser',
+  async () => {
+        const res = await UserService.getCurretUser();
+    return res.data;
+  }
+)
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    resetUser: state => {
+      state.user = null
+      state.status = 'fulfilled'
+      state.isLoading = false
+    },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchCurrentUser.pending, state => {
+        state.status = 'pending'
+        state.isLoading = true
+
+      })
+      .addCase(
+        fetchCurrentUser.fulfilled,
+        (state, action: PayloadAction<UserData>) => {
+          state.status = 'fulfilled'
+          state.user = action.payload
+          state.isLoading = false
+        }
+      )
+      .addCase(fetchCurrentUser.rejected, (state) => {
+        state.status = 'rejected'
+        state.isLoading = false,
+        state.user = null
+      })
+  },
+})
+
+export const { resetUser } = userSlice.actions
+export default userSlice.reducer
